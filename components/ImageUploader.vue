a3334a7e739d580e2d045ad3ce17e546203a8088
moises <mo.sepulvedar@gmail.com>
2022-01-17 21:45:32 -0300

    cloudinary module added

--- components/ImageUploader.vue
+++ components/ImageUploader.vue
@@ -5,6 +5,8 @@
 </template>
 
 <script>
+import {unWrap} from "~/utils/fetchUtils";
+
 export default {
   name: "ImageUploader",
   methods: {
@@ -14,7 +16,18 @@ export default {
       const fileName = file.name.split('.').slice(0,-1).join(',') + Date().now()
       const options = {
         timestamp: Date.now(),
+        public_id: filename,
       }
+
+      const response = await unWrap(this.$axios.post('/api/cloudinary/signature',
+          options, {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+      }))
+
+      const signature = response.json.signature
+      console.log(signature);
 
     }
   }
